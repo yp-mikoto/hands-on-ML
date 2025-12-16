58eddc2ebc33925f874ef0414fd477fdf452a666
Youngha Park <yp.mikoto@icloud.com>
2025-12-16 21:52:27 +0900

    complete: ch01

---
+++ ch01/happy_cyprus.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+data_root = "https://github.com/ageron/data/raw/main/"
+lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
+X = lifesat[["GDP per capita (USD)"]].values
+y = lifesat[["Life satisfaction"]].values
+
+lifesat.plot(kind="scatter", grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500, 62_500, 4, 9])
+plt.show()
+
+model = LinearRegression()
+
+model.fit(X, y)
+
+X_new = [[37_655.2]]
+print(model.predict(X_new))
